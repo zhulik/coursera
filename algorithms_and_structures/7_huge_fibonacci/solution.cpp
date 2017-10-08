6126fb53f0abe6e55523e7df35207c256e40d71b
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-10-08 11:43:22 -0700

    forgotten code added

--- algorithms_and_structures/7_huge_fibonacci/solution.cpp
+++ algorithms_and_structures/7_huge_fibonacci/solution.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+template<typename T> T pisano_period_length(T m) {
+    T a = 0, b = 1, tmp = 0;
+    for (T i = 0; i < m * m; i++) {
+        tmp = (a + b) % m;
+        a = b;
+        b = tmp;
+        if (a == 0 && b == 1) return i + 1;
+    }
+}
+
 template<typename T> T get_fibonacci_huge_fast(T n, T m) {
     auto pisano_length =  pisano_period_length(m);
     n = n % 60;
