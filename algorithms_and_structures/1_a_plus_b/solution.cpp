3bc1974d2064af6e23c947a4c5752bdfb1f2c472
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-10-08 17:23:36 +0300

    algorithms_and_structures first week

---
+++ algorithms_and_structures/1_a_plus_b/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a = 0;
+  int b = 0;
+
+  cin >> a;
+  cin >> b;
+
+  auto sum = a + b;
+  cout << sum << endl;
+  return 0;
+}
---
+++ algorithms_and_structures/2_max_pariwise_product/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename T> T maxPairwiseProduct(const vector<T>& numbers) {
+  int n = numbers.size();
+  int max_index1 = -1;
+    for (int i = 0; i < n; ++i)
+      if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+        max_index1 = i;
+
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+      if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        max_index2 = j;
+  return numbers[max_index1] * numbers[max_index2];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<long long> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+
+    auto result = maxPairwiseProduct(numbers);
+    cout << result << endl;
+    return 0;
+}
