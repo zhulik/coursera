468858da790806b6ad88b1a0fb77c39e45bc625d
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-10-22 13:29:12 -0700

    naive 21_closest

--- general_data_structures_and_algorithms/algorithmic_toolbox/20_points_and_segments/solution.cpp
+++ general_data_structures_and_algorithms/algorithmic_toolbox/20_points_and_segments/solution.cpp
@@ -1,19 +1,92 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <tuple>
 
-using std::vector;
+using namespace std;
 
-vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  //write your code here
+template<typename T>
+class Range {
+public:
+  Range() {};
+  Range(T start, T end) : m_start(start), m_end(end) {}
+
+  T start() const { return m_start; }
+  T end() const { return m_end; }
+
+private:
+  T m_start = 0;
+  T m_end = 0;
+};
+
+template<typename T>
+class MarkedPoint {
+public:
+  enum Position {
+    Left = 0,
+    Point = 1,
+    Right = 2
+  };
+
+  MarkedPoint() {}
+  MarkedPoint(T point, Position position, int point_pos = -1): m_point(point), m_position(position), m_point_pos(point_pos) {}
+  bool operator<(const MarkedPoint<T> &other) const { return tie(m_point, m_position) < tie(other.m_point, other.m_position); }
+
+  T point() const { return m_point; };
+  Position position() const { return m_position; }
+  int index() const { return m_point_pos; }
+
+private:
+  T m_point = 0;
+  Position m_position = Left;
+  int m_point_pos = -1;
+};
+
+template<typename T>
+vector<MarkedPoint<T>> markedPoints(const vector<Range<T>> &ranges, const vector<T> &points) {
+  vector<MarkedPoint<T>> res(ranges.size() * 2 + points.size());
+  int pos = 0;
+  for(auto r: ranges) {
+    res[pos++] = MarkedPoint<T>(r.start(), MarkedPoint<T>::Left);
+    res[pos++] = MarkedPoint<T>(r.end(), MarkedPoint<T>::Right);
+  }
+  for(auto i = 0; i < points.size(); i++) {
+    res[pos++] = MarkedPoint<T>(points[i], MarkedPoint<T>::Point, i);
+  }
+
+  sort(res.begin(), res.end());
+
+  return res;
+}
+
+
+template<typename T>
+vector<T> fast_count_segments(const vector<Range<T>> &ranges, const vector<T> &points) {
+  vector<T> cnt(points.size(), 0);
+  int c = 0;
+
+  auto pps = markedPoints(ranges, points);
+  for(auto p: pps) {
+    if (p.position() == MarkedPoint<T>::Left) {
+      c++;
+    }
+    if (p.position() == MarkedPoint<T>::Right) {
+      c--;
+    }
+    if (p.position() == MarkedPoint<T>::Point) {
+      cnt[p.index()] = c;
+    }
+  }
+
   return cnt;
 }
 
-vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
-  vector<int> cnt(points.size());
-  for (size_t i = 0; i < points.size(); i++) {
-    for (size_t j = 0; j < starts.size(); j++) {
-      cnt[i] += starts[j] <= points[i] && points[i] <= ends[j];
+template<typename T>
+vector<T> naive_count_segments(const vector<Range<T>> &ranges, const vector<T> &points) {
+  vector<T> cnt(points.size());
+  for (int i = 0; i < points.size(); i++) {
+    for (auto r: ranges) {
+      cnt[i] += r == points[i];
     }
   }
   return cnt;
@@ -21,18 +94,25 @@ vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<in
 
 int main() {
   int n, m;
-  std::cin >> n >> m;
-  vector<int> starts(n), ends(n);
-  for (size_t i = 0; i < starts.size(); i++) {
-    std::cin >> starts[i] >> ends[i];
+  cin >> n >> m;
+
+  vector<long long> starts(n), ends(n);
+  for (int i = 0; i < starts.size(); i++) {
+    cin >> starts[i] >> ends[i];
   }
-  vector<int> points(m);
-  for (size_t i = 0; i < points.size(); i++) {
-    std::cin >> points[i];
+
+  vector<long long> points(m);
+  for (int i = 0; i < points.size(); i++) {
+    cin >> points[i];
   }
-  //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
-  for (size_t i = 0; i < cnt.size(); i++) {
-    std::cout << cnt[i] << ' ';
+
+  vector<Range<long long>> ranges(n);
+  for (int i = 0; i < n; i++) {
+    ranges[i] = Range<long long>(starts[i], ends[i]);
+  }
+
+  auto cnt = fast_count_segments(ranges, points);
+  for (auto c: cnt) {
+    cout << c << " ";
   }
 }
--- general_data_structures_and_algorithms/algorithmic_toolbox/21_closest/solution.cpp
+++ general_data_structures_and_algorithms/algorithmic_toolbox/21_closest/solution.cpp
@@ -1,30 +1,59 @@
-#include <algorithm>
 #include <iostream>
-#include <sstream>
 #include <iomanip>
 #include <vector>
-#include <string>
 #include <cmath>
 
-using std::vector;
-using std::string;
-using std::pair;
-using std::min;
+using namespace std;
 
+template<typename T>
+class Point {
+public:
+  Point(){};
+  Point(T x, T y, size_t id): m_x(x), m_y(y), m_id(id) {}
+  double distance(const Point<T> &other) const {
+    return sqrt(pow(other.m_x - m_x, 2) + pow((other.m_y - m_y), 2));
+  }
 
-double minimal_distance(vector<int> x, vector<int> y) {
-  //write your code here
-  return 0.;
+  bool operator==(const Point<T> &other) const {
+    return m_id == other.m_id;
+  }
+
+  bool operator!=(const Point<T> &other) const {
+    return !((*this) == other);
+  }
+
+private:
+  T m_x = 0;
+  T m_y = 0;
+  size_t m_id = -1;
+};
+
+double minimal_distance(const vector<Point<long long>> &points) {
+  double min = points[0].distance(points[1]);
+
+  for(auto a: points){
+    for(auto b: points){
+      if (a.distance(b) < min && a != b) {
+        min = a.distance(b);
+      }
+    }
+  }
+
+  return min;
 }
 
 int main() {
   size_t n;
-  std::cin >> n;
+  cin >> n;
   vector<int> x(n);
   vector<int> y(n);
   for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i] >> y[i];
+    cin >> x[i] >> y[i];
   }
-  std::cout << std::fixed;
-  std::cout << std::setprecision(9) << minimal_distance(x, y) << "\n";
+  vector<Point<long long>> points(n);
+  for (size_t i = 0; i < n; i++) {
+    points[i] = Point<long long>(x[i], y[i], i);
+  }
+  cout << fixed;
+  cout << setprecision(9) << minimal_distance(points) << endl;
 }
