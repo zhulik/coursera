1c177c477cc14eaa02caf2376d36532c9ed460fd
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-10-22 05:34:02 -0700

    improvements

--- general_data_structures_and_algorithms/algorithmic_toolbox/19_inversions/solution.cpp
+++ general_data_structures_and_algorithms/algorithmic_toolbox/19_inversions/solution.cpp
@@ -1,74 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
 typedef vector<int> Array;
-
-int merge(Array &arr, int l, int m, int r)
-{
-    int count = 0;
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 =  r - m;
-
-    int L[n1], R[n2];
-
-    for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1+ j];
-
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if (L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
-        else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < n1)
-    {
-        arr[k] = L[i];
-        count++;
-        i++;
-        k++;
-    }
-
-    while (j < n2)
-    {
-        arr[k] = R[j];
-        count++;
-        j++;
-        k++;
-    }
-    return count;
-}
-
-int mergeSort(Array &arr, int l, int r, int count = 0)
-{
-    if (l < r)
-    {
-        int m = (l + r) / 2;
-
-        mergeSort(arr, l, m, count);
-        mergeSort(arr, m + 1, r, count);
-
-        count += merge(arr, l, m, r);
-    }
-    return count;
-}
 
 void print_array(const Array &a) {
   for (auto n: a) {
@@ -77,14 +13,79 @@ void print_array(const Array &a) {
   cout << endl;
 }
 
-int main() {
-  int n;
-  cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    cin >> a[i];
+int merge(int arr[], int temp[], int left, int mid, int right) {
+  int inv_count = 0;
+
+  int i = left;
+  int j = mid;
+  int k = left;
+  while ((i <= mid - 1) && (j <= right))
+  {
+  	if (arr[i] <= arr[j])
+  	{
+  	temp[k++] = arr[i++];
+  	}
+  	else
+  	{
+  	temp[k++] = arr[j++];
+
+  	inv_count = inv_count + (mid - i);
+  	}
   }
-  print_array(a);
-  cout << mergeSort(a, 0, a.size()-1) << endl;
-  print_array(a);
+
+  while (i <= mid - 1)
+  	temp[k++] = arr[i++];
+
+  while (j <= right)
+  	temp[k++] = arr[j++];
+
+  for (i=left; i <= right; i++)
+  	arr[i] = temp[i];
+
+  return inv_count;
+}
+
+int mergeSort(int arr[], int temp[], int left, int right)
+{
+  int mid, inv_count = 0;
+  if (right > left)
+  {
+  	mid = (right + left) / 2;
+
+  	inv_count = mergeSort(arr, temp, left, mid);
+  	inv_count += mergeSort(arr, temp, mid+1, right);
+
+  	inv_count += merge(arr, temp, left, mid+1, right);
+  }
+  return inv_count;
+}
+
+int mergeSort(int arr[], int array_size)
+{
+	int *temp = (int *)malloc(sizeof(int)*array_size);
+	return mergeSort(arr, temp, 0, array_size - 1);
+}
+
+void stressTest() {
+  int arr1[] = {1, 20, 6, 4, 5};
+  assert(mergeSort(arr1, 5) == 5);
+  int arr2[] = {9, 9, 3, 3, 3, 3, 3, 3, 1, 1 };
+  assert(mergeSort(arr2, 10) == 28);
+
+  int arr3[] = {2, 3, 9, 2, 9 };
+  assert(mergeSort(arr3, 5) == 2);
+}
+
+
+int main() {
+  stressTest();
+  // int n;
+  // cin >> n;
+  // Array a(n);
+  // for (size_t i = 0; i < a.size(); i++) {
+  //   cin >> a[i];
+  // }
+  // print_array(a);
+  // cout << mergeSort(&a[0], a.size()) << endl;
+  // print_array(a);
 }
