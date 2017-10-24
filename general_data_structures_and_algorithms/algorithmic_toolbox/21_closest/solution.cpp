9fe5ee5cfc923cca16c59e5008f7e6ad89c636b7
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-10-24 02:28:48 -0700

    21_closest fixed

--- general_data_structures_and_algorithms/algorithmic_toolbox/21_closest/solution.cpp
+++ general_data_structures_and_algorithms/algorithmic_toolbox/21_closest/solution.cpp
@@ -1,31 +1,22 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
 #include <algorithm>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-template<typename T>
-class Point {
+template <typename T> class Point {
 public:
   Point(){};
-  Point(T x, T y, size_t id): m_x(x), m_y(y), m_id(id) {}
+  Point(T x, T y, size_t id) : m_x(x), m_y(y), m_id(id) {}
   double distance(const Point<T> &other) const {
     return sqrt(pow(other.m_x - m_x, 2) + pow((other.m_y - m_y), 2));
   }
 
-  bool operator==(const Point<T> &other) const {
-    return m_id == other.m_id;
-  }
+  bool operator==(const Point<T> &other) const { return m_id == other.m_id; }
 
-  bool operator!=(const Point<T> &other) const {
-    return !((*this) == other);
-  }
-
-  bool operator<(const Point<T> &other) const {
-    return m_x < other.m_x;
-  }
+  bool operator!=(const Point<T> &other) const { return !((*this) == other); }
 
   T x() const { return m_x; }
   T y() const { return m_y; }
@@ -36,27 +27,11 @@ private:
   size_t m_id = -1;
 };
 
-template<typename T, typename M>
-double median(vector<Point<T>> points, M m)
-{
-  double median;
-  size_t size = points.size();
-
-  if (size  % 2 == 0) {
-      median = (double)((points[size / 2 - 1].*m)() + (points[size / 2].*m)()) / 2;
-  }
-  else {
-      median = points[size / 2].x();
-  }
-
-  return median;
-}
-
-double minimal_distance(const vector<Point<long long>> &points) {
+template <typename T> double brute(const vector<Point<T>> &points) {
   double min = points[0].distance(points[1]);
 
-  for(auto a: points){
-    for(auto b: points){
+  for (auto a : points) {
+    for (auto b : points) {
       if (a.distance(b) < min && a != b) {
         min = a.distance(b);
       }
@@ -64,6 +39,40 @@ double minimal_distance(const vector<Point<long long>> &points) {
   }
 
   return min;
+}
+
+template <typename T> double strip_closest(const vector<Point<T>> &strip, double d) {
+  auto min = d;
+  for (int i = 0; i < strip.size(); ++i)
+        for (int j = i+1; j < strip.size() && (strip[j].y() - strip[i].y()) < min; ++j)
+            if (strip[i].distance(strip[j]) < min)
+                min = strip[i].distance(strip[j]);
+  return min;
+}
+
+template <typename T> double closest(const vector<Point<T>> &points) {
+  if (points.size() <= 3) {
+    return brute(points);
+  }
+
+  int mid = points.size() / 2;
+  auto midPoint = points[mid];
+  vector<Point<T>> lp(points.begin(), points.begin() + mid);
+  vector<Point<T>> rp(points.begin() + mid + 1, points.end());
+
+  auto lc = closest(lp);
+  auto rc = closest(rp);
+
+  auto d = min(lc, rc);
+
+  vector<Point<T>> strip;
+  int j = 0;
+  for (size_t i = 0; i < points.size(); i++)
+    if (abs(points[i].x() - midPoint.x()) < d)
+      strip.push_back(points[i]);
+  std::sort(strip.begin(), strip.end(), [](const auto &f, const auto &s) { return f.y() < s.y(); });
+  auto sc = strip_closest(strip, d);
+  return min(d, sc);
 }
 
 int main() {
@@ -78,8 +87,7 @@ int main() {
   for (size_t i = 0; i < n; i++) {
     points[i] = Point<long long>(x[i], y[i], i);
   }
-  std::sort(points.begin(), points.end());
-  cout << median(points, &Point<long long>::x) << endl;
+  std::sort(points.begin(), points.end(), [](const auto &f, const auto &s) { return f.x() < s.x(); });
   cout << fixed;
-  cout << setprecision(9) << minimal_distance(points) << endl;
+  cout << setprecision(9) << closest(points) << endl;
 }
