dab19775209bed40024fc216dbcfb83fe28d6cd4
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-11-05 14:10:34 -0800

    26_lcs3 fixed. algorithmic_toolbox is done

--- general_data_structures_and_algorithms/algorithmic_toolbox/26_lcs3/solution.cpp
+++ general_data_structures_and_algorithms/algorithmic_toolbox/26_lcs3/solution.cpp
@@ -4,29 +4,27 @@
 
 using namespace std;
 
-template <typename T> T lcs(const vector<T> X, const vector<T> Y) {
-  T m = X.size();
-  T n = Y.size();
-  vector<vector<T>> L(m + 1, vector<T>(n + 1, 0));
+template <typename T>
+T lcs3(const vector<T> X, const vector<T> Y, const vector<T> Z) {
+  int m = X.size();
+  int n = Y.size();
+  int o = Z.size();
+  vector<vector<vector<T>>> L(m + 1,
+                              vector<vector<T>>(n + 1, vector<T>(o + 1, 0)));
 
   for (int i = 0; i <= m; i++) {
     for (int j = 0; j <= n; j++) {
-      if (i == 0 || j == 0)
-        L[i][j] = 0;
-
-      else if (X[i - 1] == Y[j - 1])
-        L[i][j] = L[i - 1][j - 1] + 1;
-
-      else
-        L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+      for (int k = 0; k <= o; k++) {
+        if (i == 0 || j == 0 || k == 0)
+          L[i][j][k] = 0;
+        else if (X[i - 1] == Y[j - 1] && X[i - 1] == Z[k - 1])
+          L[i][j][k] = L[i - 1][j - 1][k - 1] + 1;
+        else
+          L[i][j][k] = max({L[i - 1][j][k], L[i][j - 1][k], L[i][j][k - 1]});
+      }
     }
   }
-
-  return L[m][n];
-}
-
-template <typename T> T lcs3(vector<T> &a, vector<T> &b, vector<T> &c) {
-  return min({lcs(a, b), lcs(b, c), lcs(a, c)});
+  return L[m][n][o];
 }
 
 int main() {
