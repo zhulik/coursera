db0cf78a79f4740b9ceaabb44fea5ea0f2280e9d
Gleb Sinyavsky <zhulik.gleb@gmail.com>
2017-11-05 08:16:17 -0800

    23_knapsack added

---
+++ general_data_structures_and_algorithms/algorithmic_toolbox/23_knapsack/solution.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T> void print(const std::vector<std::vector<T>> &tries) {
+  for(auto row: tries) {
+    for(auto item: row) {
+      cout << item << " ";
+    }
+    cout << endl;
+  }
+}
+
+template<typename T>
+T optimal_weight(T W, const vector<T> &items) {
+  T n = items.size();
+  std::vector<std::vector<T>>tries(W + 1, std::vector<int>(n + 1, 0));
+
+
+  for(T i = 1; i <= n; i++ ) {
+    for(T w = 1; w <= W; w++) {
+      tries[w][i] = tries[w][i - 1];
+      if (items[i-1] <= w) {
+        T val = tries[w - items[i-1]][i-1] + items[i-1];
+        if (tries[w][i] < val)
+          tries[w][i] = val;
+      }
+    }
+  }
+  return tries[W][n];
+}
+
+int main() {
+  int n, W;
+  cin >> W >> n;
+  vector<int> w(n);
+  for (int i = 0; i < n; i++) {
+    cin >> w[i];
+  }
+  cout << optimal_weight(W, w) << endl;
+}
